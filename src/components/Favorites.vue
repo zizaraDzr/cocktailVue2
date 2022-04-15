1c1023e770e817c0e02c95e7d2a1a234f3cda22d
zizaraDzr <54453367+zizaraDzr@users.noreply.github.com>
2022-04-15 13:56:08 +0300

    Fav branch+
    * add favorites
    
    * fix
    
    * main page
    
    * add favorites
    
    * fix
    
    * main page
    
    * add favorites
    
    * fix
    
    * main page
    
    * add favorites
    
    * main page

---
+++ src/components/Favorites.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="content" class="mt-5">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="jumbotron col-12 col-md-10">
+          <h1 class="text-center mb-4">My Favorites</h1>
+          <div class="row">
+            <div class="col-12">
+              <table id="favorites" class="table table-light">
+                <thead class="bg-danger">
+                  <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">View</th>
+                    <th scope="col">Remove</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FavoritesComponents',
+  }
+</script>
+
+<style lang="scss" scoped></style>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,14 +4,17 @@ import Cocktail from '@/components/Cocktail.vue'
 import Ingredient from '@/components/Ingredient.vue'
 import Category from '@/components/Category.vue'
 import Alcohol from '@/components/Alcohol.vue'
+import Favorites from '@/components/Favorites.vue'
 
 Vue.use(VueRouter)
+
 const router = new VueRouter({
     routes: [
         { path: '/Cocktail', component: Cocktail },
         { path: '/Ingredient', component: Ingredient },
         { path: '/Category', component: Category },
-        { path: '/Alcohol', component: Alcohol }
+        { path: '/Alcohol', component: Alcohol },
+        { path: '/Favorites', component: Favorites }
     ]
   })
 export default router
